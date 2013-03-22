37c91f108b6f03090a2ad6b7cc7ca0460e07ce96
webworkzs <nelotpalpadhan@gmail.com>
2013-03-22 13:35:30 -0700

    Android and IOS Socaial Networking Plugins Merged - Mak Kaloliya

--- Zombie Blaster/Assets/Plugins/SocialNetworking/FacebookAndroid.cs
+++ Zombie Blaster/Assets/Plugins/SocialNetworking/FacebookAndroid.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Prime31;
 
 
 #if UNITY_ANDROID
+public enum FacebookSessionDefaultAudience
+{
+	NONE,
+	ONLY_ME,
+	FRIENDS,
+	EVERYONE
+}
+
 public class FacebookAndroid
 {
 	private static AndroidJavaObject _facebookPlugin;
@@ -23,18 +32,16 @@ public class FacebookAndroid
 		{
 			Facebook.instance.accessToken = getAccessToken();
 		};
-
 	}
 
 	
-	// Initializes the Facebook object with your appId.  Returns true if the user is already authenticated.
-	public static void init( string appId )
+	// Initializes the Facebook plugin for your application
+	public static void init()
 	{
 		if( Application.platform != RuntimePlatform.Android )
 			return;
 
-		_facebookPlugin.Call( "init", appId );
-		Facebook.instance.accessToken = getAccessToken();
+		_facebookPlugin.Call( "init" );
 	}
 	
 	
@@ -54,53 +61,68 @@ public class FacebookAndroid
 		if( Application.platform != RuntimePlatform.Android )
 			return string.Empty;
 			
-		return _facebookPlugin.Call<string>( "getSessionToken" );
+		return _facebookPlugin.Call<string>( "getAccessToken" );
 	}
 	
 	
-	// Extends the logged in user's access token
-	public static void extendAccessToken()
+	// Gets the permissions granted to the current access token
+	public static List<object> getSessionPermissions()
 	{
-		if( Application.platform != RuntimePlatform.Android )
-			return;
-			
-		_facebookPlugin.Call( "extendAccessToken" );
+		if( Application.platform == RuntimePlatform.Android )
+		{
+			var permissions = _facebookPlugin.Call<string>( "getSessionPermissions" );
+			return permissions.listFromJson();
+		}
+		
+		return new List<object>();
 	}
-	
 
-	// Shows the login dialog with specific permissions
+
 	public static void login()
 	{
-		loginWithRequestedPermissions( new string[] {} );
+		loginWithReadPermissions( new string[] {} );
 	}
 	
-	// for iOS compatibility
-	public static void loginWithRequestedPermissions( string[] permissions, string urlSchemeSuffix )
-	{
-		loginWithRequestedPermissions( permissions );
-	}
 
-	public static void loginWithRequestedPermissions( string[] permissions )
+	// Authenticates the user requesting the passed in read permissions
+	public static void loginWithReadPermissions( string[] permissions )
 	{
 		if( Application.platform != RuntimePlatform.Android )
 			return;
 
-		_facebookPlugin.Call( "showLoginDialog", new object[] { permissions } );
-		//var loginMethod = AndroidJNI.GetMethodID( _facebookPlugin.GetRawClass(), "showLoginDialog", "([Ljava/lang/String;)V" );
-		//AndroidJNI.CallVoidMethod( _facebookPlugin.GetRawObject(), loginMethod, AndroidJNIHelper.CreateJNIArgArray( new object[] { permissions } ) );
+		_facebookPlugin.Call( "loginWithReadPermissions", new object[] { permissions } );
 	}
-	
-	
-	// Logs in a user attempting to use SSO if the Facebook app is available
-	public static void loginWithSingleSignOn( string[] permissions )
+
+
+	// Authenticates the user requesting the passed in publish permissions
+	public static void loginWithPublishPermissions( string[] permissions )
 	{
 		if( Application.platform != RuntimePlatform.Android )
 			return;
 
-		var loginMethod = AndroidJNI.GetMethodID( _facebookPlugin.GetRawClass(), "showSSOLoginDialog", "([Ljava/lang/String;)V" );
-		AndroidJNI.CallVoidMethod( _facebookPlugin.GetRawObject(), loginMethod, AndroidJNIHelper.CreateJNIArgArray( new object[] { permissions } ) );
+		_facebookPlugin.Call( "loginWithPublishPermissions", new object[] { permissions } );
 	}
 
+	
+	// Reauthorizes with the requested read permissions
+	public static void reauthorizeWithReadPermissions( string[] permissions )
+	{
+		if( Application.platform != RuntimePlatform.Android )
+			return;
+
+		_facebookPlugin.Call( "reauthorizeWithReadPermissions", permissions.toJson() );
+	}
+	
+	
+	// Reauthorizes with the requested publish permissions and audience
+	public static void reauthorizeWithPublishPermissions( string[] permissions, FacebookSessionDefaultAudience defaultAudience )
+	{
+		if( Application.platform != RuntimePlatform.Android )
+			return;
+
+		_facebookPlugin.Call( "reauthorizeWithPublishPermissions", permissions.toJson(), defaultAudience.ToString() );
+	}
+	
 
 	// Logs the user out and invalidates the token
 	public static void logout()
@@ -111,35 +133,14 @@ public class FacebookAndroid
 		_facebookPlugin.Call( "logout" );
 		Facebook.instance.accessToken = string.Empty;
 	}
-
-
-	// Shows the Facebook post message dialog
-	public static void showPostMessageDialog()
-	{
-		showDialog( "stream.publish", null );
-	}
-
-
-	// Shows the Facebook post message dialog with additional options.  Pass null or string.empty for those that you do not want to include
-	public static void showPostMessageDialogWithOptions( string link, string linkName, string linkToImage, string caption )
-	{
-		var parameters = new Dictionary<string,string>
-		{
-			{ "link", link },
-			{ "name", linkName },
-			{ "picture", linkToImage },
-			{ "caption", caption }
-		};
-		showDialog( "stream.publish", parameters );
-	}
 	
-	
+
 	// Full access to any existing or new Facebook dialogs that get added.  See Facebooks documentation for parameters and dialog types
 	public static void showDialog( string dialogType, Dictionary<string,string> parameters )
 	{
 		if( Application.platform != RuntimePlatform.Android )
 			return;
-
+		
 		// load up the Bundle
 		using( var bundle = new AndroidJavaObject( "android.os.Bundle" ) )
 		{
@@ -162,37 +163,6 @@ public class FacebookAndroid
 		}
 	}
 
-
-	// Calls a custom Rest API method with the key/value pairs in the Dictionary.  Pass in a null dictionary if no parameters are needed.
-	public static void restRequest( string restMethod, string httpMethod, Dictionary<string,string> parameters )
-	{
-		if( Application.platform != RuntimePlatform.Android )
-			return;
-		
-		// add in the method
-		if( parameters == null )
-			parameters = new Dictionary<string, string>();
-		parameters.Add( "method", restMethod );
-		
-		// load up the Bundle
-		using( var bundle = new AndroidJavaObject( "android.os.Bundle" ) )
-		{
-			var putStringMethod = AndroidJNI.GetMethodID( bundle.GetRawClass(), "putString", "(Ljava/lang/String;Ljava/lang/String;)V" );
-			var args = new object[2];
-			
-			// add all our dictionary elements into the Bundle
-			foreach( var kv in parameters  )
-			{
-				args[0] = new AndroidJavaObject( "java.lang.String", kv.Key );
-				args[1] = new AndroidJavaObject( "java.lang.String", kv.Value );
-				AndroidJNI.CallVoidMethod( bundle.GetRawObject(), putStringMethod, AndroidJNIHelper.CreateJNIArgArray( args ) );
-			}
-			
-			// call off to java land
-			_facebookPlugin.Call( "restRequest", httpMethod, bundle );
-		}
-	}
-	
 	
 	// Calls a custom Graph API method with the key/value pairs in the Dictionary.  Pass in a null dictionary if no parameters are needed.
 	public static void graphRequest( string graphPath, string httpMethod, Dictionary<string,string> parameters )
@@ -221,7 +191,6 @@ public class FacebookAndroid
 			_facebookPlugin.Call( "graphRequest", graphPath, httpMethod, bundle );
 		}
 	}
-
-
+	
 }
 #endif
